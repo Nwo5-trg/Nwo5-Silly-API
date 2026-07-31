669420e6a9be4695e756d41a718aa2ed13320f1f
Fei <cubecereal@gmail.com>
2026-07-31 11:01:59 -0400

    i

--- include/settings/setting.hpp
+++ include/settings/setting.hpp
@@ -226,7 +226,7 @@ namespace nwo5::settings {
 
             geode::SettingChangedEventV3(m_mod, std::move(key())).listen([this] (std::shared_ptr<geode::SettingV3> pSetting) {
                 if (auto ty = geode::cast::typeinfo_pointer_cast<typename geode::SettingTypeForValueType<T>::SettingType>(pSetting)) {
-                    return m_value = ty->getValue();
+                    m_value = ty->getValue();
                 }
             }, geode::Priority::First).leak();
 
